9cdea00e2196d6328adbcc797833611dcf1f59af
Aja Singer <ajasinger@gmail.com>
2024-10-21 11:10:02 -0400

    computer player updates

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 //During your interview, you will pair on adding support for a computer player to your game. 
 //You can start with random moves and make the AI smarter if you have time.
 
+// add 2-player button & computer button 
+// set a new game 
+// a new state computerGame
+// after a move check if it's a computerGame and make move 
+// for computerMove check which squares are free by mapping through squares array 
+// record which squares are empty and assign a random index from that 
+// pass in randomIndex to handleSquareClick
+
+
 const player1 = 'X';
 const player2 = 'O';
+let computerPlayer = false;
 let currentPlayer = player1;
 const squares = new Array(9).fill('');
 const board = document.getElementById('board');
 const gameStatus = document.getElementById('gameStatus').querySelector('h2');
-const resetButton = document.getElementById('resetButton').querySelector('button');
+//const resetButton = document.getElementById('resetButton').querySelector('button');
+
+document.getElementById('humanMode').addEventListener('click', () => {
+    computerPlayer = false;
+    newGame();
+});
+
+document.getElementById('computerMode').addEventListener('click', () => {
+    computerPlayer = true;
+    newGame();
+});
 
 const winningCombinations = [
     [0, 1, 2],
@@ -34,7 +54,7 @@ const newGame = () => {
 }
 
 // Start the game on page load
-newGame();
+//newGame();
 
 const handleSquareClick = (index) => {
     //if square is blank
@@ -50,8 +70,28 @@ const handleSquareClick = (index) => {
         } else {
             currentPlayer = currentPlayer === player1 ? player2 : player1;
             gameStatus.textContent = `${currentPlayer}'s turn`;
+            if(computerPlayer && player2) {
+                computerMove();
+            }
         }
     }
+}
+
+const computerMove = () => {
+    // for computerMove check which squares are free by mapping through squares array 
+    // record which squares are empty and assign a random index from that 
+    // pass in randomIndex to handleSquareClick
+    let emptyIndex = [];
+    squares.map((square, index)=> {
+        console.log('square', square);
+        if(square === '') {
+            emptyIndex.push(index);
+        }
+    })
+    console.log('emptyIndex', emptyIndex);
+    // const emptySquares = squares.filter(square => {
+
+    // })
 }
 
 const isWin = () => {
@@ -72,4 +112,4 @@ const disableBoard = () => {
 }
 
 // Reset the game
-resetButton.addEventListener('click', newGame);
+//resetButton.addEventListener('click', newGame);
